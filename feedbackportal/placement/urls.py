b89430f545c1dcce306e076c7cc0e2abcfd98807
navinprasandh <navinprasandh@gmail.com>
2023-02-08 14:25:20 +0530

    Version.0.1.1

--- feedbackportal/placement/urls.py
+++ feedbackportal/placement/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('placement/', views.placement, name='placement'),
+    path('main/', views.detail, name='detail'),
 ]
--- feedbackportal/placement/views.py
+++ feedbackportal/placement/views.py
@@ -3,5 +3,13 @@ from django.http import HttpResponse
 from django.template import loader
 
 def placement(request):
-    template = loader.get_template('homepage.html')
+    template = loader.get_template('main/homepage.html')
     return HttpResponse(template.render())
+
+def detail(request):
+    template = loader.get_template('main/home.html')
+    return HttpResponse(template.render())
+   
+
+def profile(request):
+    return render(request,"main/profile.html")
