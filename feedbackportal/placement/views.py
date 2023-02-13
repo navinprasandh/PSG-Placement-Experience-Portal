1f7e2f8a6e7fcb29288197357c34fc1d3d310d54
navinprasandh <navinprasandh@gmail.com>
2023-02-13 11:06:28 +0530

    Version0.0.5

--- feedbackportal/placement/views.py
+++ feedbackportal/placement/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Carausel
 from django.template import loader
+
 
 def placement(request):
     template = loader.get_template('main/homepage.html')
     return HttpResponse(template.render())
 
 def main(request):
-    template = loader.get_template('main/home.html')
-    return HttpResponse(template.render())
+    obj = Carausel.objects.all()
+    context = {
+        'obj':obj
+    }
+    return render(request,'main/home.html',context)
+  
 
 def detail(request):
     template = loader.get_template('main/detail.html')
