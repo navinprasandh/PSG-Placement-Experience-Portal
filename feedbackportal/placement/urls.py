c729d4369a314fcfa42d28c32ceabccc63591dc6
navinprasandh <navinprasandh@gmail.com>
2023-02-28 10:22:05 +0530

    error

--- feedbackportal/placement/urls.py
+++ feedbackportal/placement/urls.py
@@ -4,9 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('placement/', views.placement, name='placement'),
     path('main/', views.main, name='main'),
     path('detail/', views.detail, name='detail'),
-    path('register/', views.register, name='register'),
     path('about/', views.about, name='about'),
 ]
--- feedbackportal/placement/views.py
+++ feedbackportal/placement/views.py
@@ -8,10 +8,6 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 
 
-def placement(request):
-    template = loader.get_template('main/homepage.html')
-    return HttpResponse(template.render())
-
 def main(request):
     obj = Carausel.objects.all()
     context = {
@@ -19,67 +15,10 @@ def main(request):
     }
     return render(request,'main/home.html',context)
   
-
 def detail(request):
     template = loader.get_template('main/detail.html')
     return HttpResponse(template.render())
-   
-def register(request):
-    return render(request,template_name="main/register.html")
 
 def about(request):
     return render(request,"main/about.html")
-
-def profile(request):
-    return render(request,"main/profile.html")
     
-def user_register(request):
-    # if this is a POST request we need to process the form data
-    template = 'main/register.html'
-   
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = RegisterForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            if User.objects.filter(username=form.cleaned_data['username']).exists():
-                return render(request, template, {
-                    'form': form,
-                    'error_message': 'Username already exists.'
-                })
-            elif User.objects.filter(email=form.cleaned_data['email']).exists():
-                return render(request, template, {
-                    'form': form,
-                    'error_message': 'Email already exists.'
-                })
-            elif form.cleaned_data['password'] != form.cleaned_data['password_repeat']:
-                return render(request, template, {
-                    'form': form,
-                    'error_message': 'Passwords do not match.'
-                })
-            else:
-                # Create the user:
-                user = User.objects.create_user(
-                    form.cleaned_data['username'],
-                    form.cleaned_data['email'],
-                    form.cleaned_data['password']
-                )
-                user.first_name = form.cleaned_data['first_name']
-                user.last_name = form.cleaned_data['last_name']
-                user.phone_number = form.cleaned_data['phone_number']
-                user.save()
-               
-                # Login the user
-                login(request, user)
-               
-                # redirect to accounts page:
-                return HttpResponseRedirect('/mymodule/account')
-
-   # No post data availabe, let's just show the page.
-    else:
-        form = RegisterForm()
-
-    return render(request, template, {'form': form})
-#def studentview(CreateView):
- #   model = Students
-  #  fields = ('name', 'rollnumber','email','department','course','yearofpassing')
