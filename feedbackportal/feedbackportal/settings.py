d1d46d0c6185dac16007e0590e5bc5226faa3dbf
navinprasandh <navinprasandh@gmail.com>
2023-02-28 10:14:09 +0530

    Version0.0.10

--- feedbackportal/feedbackportal/settings.py
+++ feedbackportal/feedbackportal/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'placement',
 ]
 
-CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
